0398c5c36cf626aafb5c5fab327ad9b73244663e
0-chenzhiyuan-0 <3461594347@qq.com>
2026-05-29 02:23:26 +0800

    增加文件上传功能

--- src/main/java/org/example/controller/UploadController.java
+++ src/main/java/org/example/controller/UploadController.java
@@ -2,6 +2,8 @@ package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.pojo.Result;
+import org.example.utils.AliyunOSSOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,16 +14,22 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class UploadController {
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
     @PostMapping("/upload")
-    public Result upload(String name,Integer age, MultipartFile file) throws IOException {
+    public Result upload(String name,Integer age, MultipartFile file) throws Exception {
         log.info("文件上传开始");
         log.info("文件名：{}",file.getOriginalFilename());
         log.info("文件大小：{}",file.getSize());
         log.info("文件类型：{}",file.getContentType());
-        String originalFileName = file.getOriginalFilename();
-        String suffix = originalFileName.substring(originalFileName.lastIndexOf("."));
-        String newFileName = UUID.randomUUID().toString() + suffix;
-        file.transferTo(new java.io.File("C:/Users/34615/Desktop/" + newFileName));
-        return Result.success();
+        if (!file.isEmpty()){
+            String originalFileName = file.getOriginalFilename();
+            String extName = originalFileName.substring(originalFileName.lastIndexOf("."));
+            String uniqueFileName = UUID.randomUUID().toString() + extName;
+            String url=aliyunOSSOperator.upload(file.getBytes(), uniqueFileName);
+
+            return Result.success(url);
+        }
+        return Result.error("上传失败");
     }
 }
